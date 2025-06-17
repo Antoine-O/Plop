f56a935f89d5c1fb574adb487a29f159db141b62
Antoine O <antoine.oberlaender@profluens.com>
2025-06-17 10:33:14 +0200

    Refactor: Update versioning in build process
    This commit refactors the versioning update mechanism during the build process.
    
    - The version update logic (using `sed`) is moved from GitHub Actions workflows (for Android and Linux) into their respective Dockerfiles.
    - The `SUBVERSION` build argument is introduced in Dockerfiles and docker-compose files for Android and Linux to pass the version number.
    - For Android local builds (`compile_android_apk_local.sh`), `SUBVERSION` is now generated using `date +%N` for nanoseconds.
    - For GitHub Actions workflows (Android and Linux), `SUBVERSION` is set to `github.run_number`.
    - For macOS and iOS GitHub Actions workflows, the `sed` command for updating the version in `pubspec.yaml` is generalized to match any existing patch version (`[0-9]+\.[0-9]+\..*`).

--- compile_android_apk_local.sh
+++ compile_android_apk_local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SUBVERSION=$(date +%N)
 docker compose  -f docker-compose-build-android.yml build
 docker compose  -f docker-compose-build-android.yml create
 docker compose cp compile-android:/app/app-release.apk ./build_output/plop-release.apk
